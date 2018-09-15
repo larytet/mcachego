338d518a7d90014c151b4949a6ccbbc88beccb95
arkady <larytet@yahoo.com>
2018-09-15 10:31:57 +0300

    API update

--- mcache.go
+++ mcache.go
@@ -8,19 +8,26 @@ import (
 
 // Straight from https://github.com/patrickmn/go-cache
 // Read also https://allegro.tech/2016/03/writing-fast-cache-service-in-go.html
-// GC is going to poll the cache entries. I can try map[init]int and cast int to
-// a (unsafe?) pointer in the arrays of strings and structures.
 type item struct {
 	Object     interface{}
 	Expiration int64
 }
 
+type itemStack struct {
+	top int64
+	data []*item
+} 
+
 type Cache struct {
+   // GC is going to poll the cache entries. I can try map[init]int and cast int to
+    // a (unsafe?) pointer in the arrays of strings and structures.
 	// I keep an address of the "item" allocated from a pool
 	data  map[string]int64
 	queue list.List
 	mutex sync.RWMutex
 	ttl int64
+	// pool of preallocted items
+	pool itemStack
 }
 
 func New(size int64, ttl int64) *Cache {
@@ -60,6 +67,9 @@ func (c *Cache) Remove(now int64) (key string, expired bool) {
 func (c *Cache) RemoveSync(now int64) (key string, expired bool) {
 }
 
+func (c *Cache) evict(now int64) (nextExpiration int64, expired bool) {
+}
+
 func (c *Cache) Evict(now int64) (nextExpiration int64, expired bool) {
 	c.mutex.Lock()
 	c.evict()
@@ -67,4 +77,9 @@ func (c *Cache) Evict(now int64) (nextExpiration int64, expired bool) {
 }
 
 func (c *Cache) EvictAll(now int64) (nextExpiration int64, expired bool) {
+	c.mutex.Lock()
+	for {
+	c.evict()
+	}
+	c.mutex.Unlock()
 }
