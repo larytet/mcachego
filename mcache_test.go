6b39e470b68824b89ad0938691bdd873436120dd
arkady <larytet@yahoo.com>
2018-09-15 13:16:14 +0300

    API update

---
+++ mcache_test.go
@@ -0,0 +1,47 @@
+package mcache
+
+import (
+	"testing"
+	"time"
+)
+
+var TTL = 1 * 1000
+var cache = New(10*1000*1000, 1*1000)
+
+func TestAdd(t *testing.T) {
+	cache.Store(0, 0)
+	v, ok := cache.Load(0)
+	if !ok {
+		t.Fatalf("Failed to load value from the cache")
+	}
+	if v != 0 {
+		t.Fatalf("Wrong value %v instead of %v", v, 0)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	time.Sleep(time.Second)
+	nextExpiration, evicted := cache.Evict(Nanotime())
+	if !evicted {
+		t.Fatalf("Failed to evict value from the cache")
+	}
+	if nextExpiration != 0 {
+		t.Fatalf("bad next expiration %v", nextExpiration)
+	}
+	_, ok := cache.Load(0)
+	if ok {
+		t.Fatalf("Failed to remove value from the cache")
+	}
+}
+
+func BenchmarkStore(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		cache.Store(Key(i), Object(i))
+	}
+}
+
+func BenchmarkLoad(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		cache.Load(Key(i))
+	}
+}
